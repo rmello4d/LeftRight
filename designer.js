46f3fcf202aa0f51a42813924f7a957bf2173466
Ricardo <ricardo.mello@4d.com>
2013-10-18 09:32:48 +0200

    First Commit

---
+++ designer.js
@@ -0,0 +1,36 @@
+(function() {
+    var LeftRight = Widget.LeftRight.inherit(WAF.require('waf-behavior/studio'));
+	
+	// adding attributes to the Studio panel
+    LeftRight.addAttributes([{
+    	'name':'data-width',
+    	'description':'Width',
+    	'defaultValue': '250px'
+    },{
+    	'name':'data-height',
+    	'description':'Height',
+    	'defaultValue': '250px'
+    }]);   
+    
+    // defining the action when user resize the widget in the Studio     
+    LeftRight.on('Resize', function(event) {
+    	this.studioCss('height', event.size.height);
+    	this.studioCss('width', event.size.width); 
+    	this.studioVal('data-width', event.size.width + 'px');
+    	this.studioVal('data-height', event.size.height + 'px');	 	
+	 	this.studioPanelRefresh();
+    });
+    
+
+    // defining the action when widget is displayed/updated in the Studio 
+	LeftRight.on('Display', function (attributes) {
+		//display width and height
+	 	this.studioCss('width', attributes['data-width']); 	
+	 	this.studioCss('height', attributes['data-height']); 		  
+	 });
+
+
+
+})();
+
+// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html
---
+++ widget.js
@@ -0,0 +1,35 @@
+(function() {
+    var widget = WAF.require('waf-core/widget');
+    var LeftRight = widget.create('LeftRight');
+  
+    //creating the widget
+    var LeftRight = widget.create('LeftRight');
+    
+    // inheriting from the composed behavior
+    LeftRight.inherit(WAF.require('waf-behavior/layout/composed'));       
+    
+       
+    // creating empty <div> widgets 
+    var simple1 = widget.create('simple1');
+    var simple2 = widget.create('simple2');
+    
+    // setting each part of the composed with an empty widget                      
+    LeftRight.setPart('left', 	simple1); 
+    LeftRight.setPart('right', 	simple2);
+  
+
+	// define the widget's init method
+ 	LeftRight.prototype.init = function() {
+    
+    	// adding a general class to the widget
+        this.addClass('waf-skin-box');
+              
+        //creating an internal event that will change the color of the content to yellow       
+ 		$(this.node).on('click', function() {
+			this.getPart('left').style('background', 'gray');
+		}.bind(this));
+	}
+
+})();
+
+// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3871.html
